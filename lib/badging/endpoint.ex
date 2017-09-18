ca760ef5a6a20424901f6c7d548754069576464e
Viktar Basharymau <viktar.basharymau@gmail.com>
2017-09-18 20:42:37 +0300

    Remove Plug.Session because it is unused

--- lib/badging/endpoint.ex
+++ lib/badging/endpoint.ex
@@ -28,14 +28,6 @@ defmodule Badging.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
 
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
-    store: :cookie,
-    key: "_badging_key",
-    signing_salt: "HEqBDa+2"
-
   plug CORSPlug, Application.get_env(:badging, :cors, [])
 
   plug Badging.Router
