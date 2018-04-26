e0cc9acbc78b3dbed5c2e35562e80c372180e8cd
Viktar Basharymau <viktar.basharymau@gmail.com>
2018-04-26 17:32:20 +0300

    Update elixir to 1.6.3

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Badging.Mixfile do
     [
       app: :badging,
       version: "0.0.1",
-      elixir: "~> 1.2",
+      elixir: "~> 1.6",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: [:phoenix, :gettext] ++ Mix.compilers(),
       build_embedded: Mix.env() == :prod,
